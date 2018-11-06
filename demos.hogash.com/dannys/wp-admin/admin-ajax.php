5c6b09fab47cc9a36f72d94b46322723c60bfcfb
Tamas Mohos <zerosuxx@gmail.com>
2018-11-06 17:56:12 +0100

    init

--- demos.hogash.com/dannys/wp-admin/admin-ajax.php
+++
@@ -1,9 +0,0 @@
-﻿<?php 
-
-	$fileName = "content" . $_POST["category"] . ".html";
-
-	$myfile = fopen($fileName, "r") or die("Unable to open file!");
-	echo fread($myfile,filesize($fileName));
-	fclose($myfile);
-
-?>
---
+++ wp-admin/admin-ajax.php
@@ -0,0 +1,9 @@
+﻿<?php 
+
+	$fileName = "content" . $_POST["category"] . ".html";
+
+	$myfile = fopen($fileName, "r") or die("Unable to open file!");
+	echo fread($myfile,filesize($fileName));
+	fclose($myfile);
+
+?>
