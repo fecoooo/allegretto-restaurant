6e5ec44e8ebc795d2abe4a8bf7a5cee04f37e408
Unknown <dajka1989@gmail.com>
2018-11-16 15:08:48 +0100

    Added breadcrumb for individual food pages

---
+++ demos.hogash.com/dannys/includeables/breadcrumb.php
@@ -0,0 +1,15 @@
+<nav role="navigation" aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs" itemprop="breadcrumb">
+   <ul class="trail-items" itemscope itemtype="http://schema.org/BreadcrumbList">
+      <meta name="numberOfItems" content="3" />
+      <meta name="itemListOrder" content="Ascending" />
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" class="trail-item trail-begin">
+         <a href="../../menu/index.html" rel="home" itemprop="item"><span itemprop="name">Étlap</span></a>
+         <meta itemprop="position" content="1" />
+      </li>
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" class="trail-item trail-end">
+         <span itemprop="item"><span itemprop="name">Termék</span></span>
+         <meta itemprop="position" content="2" />
+      </li>
+   </ul>
+</nav>
+<hr class="dn-breadcrumbsSep">
