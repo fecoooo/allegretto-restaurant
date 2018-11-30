eb83a80da0ca399c30faeca20d5caa4faa3fd249
Szilard Takacs <szilard.takacs87@gmail.com>
2018-11-30 11:41:53 +0100

    header change php-val generalva

--- demos.hogash.com/dannys/reservation-and-content/index.php
+++ demos.hogash.com/dannys/reservation-and-content/index.php
@@ -118,43 +118,7 @@
                </svg>
             </a>
          </div>
-         <header id="site-header" class="dn-siteHeader  dn-siteHeader--style1 dn-stickyHeader dn-stickyHeader--off  dn-stickyHeader--resize dn-siteHeader--pos-relative" role="banner" itemscope="itemscope" itemtype="https://schema.org/WPHeader" >
-            <div class="dn-siteHeader-wrapper">
-               <div class="dn-siteHeader-main">
-                  <div class="container dn-siteHeader-container">
-                     <div class="fxb-row dn-siteHeader-row dn-siteHeader-mainRow ">
-                        <div class="dn-siteHeader-mainLeft fxb-col fxb fxb-start-x fxb-center-y fxb-basis-50 fxb-xs-basis-auto fxb-xl-forth">
-                           <div class="dn-logoImg-wrapper dn-logoSize--custom " id="dn-logo"><a href="../index.html" class="dn-logoImg-anch"><img class="dn-logoImg dn-logoMain" src="../wp-content/uploads/sites/4/2017/05/logo.png"  alt="Dannys Restaurant" title="Dannys Restaurant | Restaurant and Cafe WordPress Theme" itemprop="logo"  width="185"  /></a></div>
-                        </div>
-                        <div class="dn-siteHeader-mainCenter fxb-col fxb fxb-center-x fxb-center-y fxb-basis-auto fxb-shrink-0">
-                           <div class="sh-component dn-mainNav-wrapper" role="navigation" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement" >
-                               <div id="dn-main-menu" class="dn-mainNav-container  zn_mega_wrapper ">
-                                 <ul id="menu-main-menu" class="dn-mainNav zn_mega_menu ">
-                                    <li id="menu-item-288" class="main-menu-item menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-288  main-menu-item-top  menu-item-even menu-item-depth-0">	<a href="../index.html" class=" main-menu-link main-menu-link-top"><span>Rólunk</span></a></li>
-                                    <li id="menu-item-292" class="main-menu-item menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-292  main-menu-item-top  menu-item-even menu-item-depth-0">											<a href="../menu/index.html" class=" main-menu-link main-menu-link-top"><span>Étlap</span></a></li>
-                                    <li id="menu-item-295" class="main-menu-item menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-295  main-menu-item-top  menu-item-even menu-item-depth-0">											<a href="../reservation-and-content/index.html" class=" main-menu-link main-menu-link-top"><span>Asztalfoglalás</span></a></li>
-                                    <li id="menu-item-491" class="main-menu-item menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-491  main-menu-item-top  menu-item-even menu-item-depth-0">											<a href="../gallery/index.html" class=" main-menu-link main-menu-link-top"><span>Galéria</span></a></li>
-                                    <li id="menu-item-491" class="main-menu-item menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-491  main-menu-item-top  menu-item-even menu-item-depth-0">											<a href="../contact-us-with-google-map/index.html" class=" main-menu-link main-menu-link-top"><span>Elérhetőségeink</span></a></li>
-                                 </ul>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="dn-siteHeader-mainRight fxb-col fxb fxb-end-x fxb-center-y fxb-basis-50 fxb-lg-basis-auto fxb-lg-shrink-0">
-                           <div class="dn-mainNavResp">
-                              <a href="#" class="dn-menuBurger" id="dn-menuBurger">
-                              <span></span>
-                              <span></span>
-                              <span></span>
-                              </a>
-                           </div>
-                           
-                           
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </header>
+         <?php include("../includeables/header.php"); ?>
          <main id="content" class="dn-siteContent dn-isPage dn-isSingle dn-isPageBuilder">
             <div class="dn-siteContainer ">
                <div class="zn_pb_wrapper clearfix zn_sortable_content" data-droplevel="0">
